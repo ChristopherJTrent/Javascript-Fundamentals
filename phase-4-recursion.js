77cdd73223a71aeb14de14692de4fb29e253d40d
shjang1025 <shjang1025@gmail.com>
2024-02-27 15:56:52 -0800

    complete part of phase 4

--- phase-4-recursion.js
+++ phase-4-recursion.js
@@ -17,3 +17,49 @@ function range(start, end, accumulator = [])  {
     }
     return range(start + 1, end, accumulator.concat(start));
 }
+
+function sumRec(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+    return arr[0] + sumRec(arr.slice(1))
+}
+
+//version1
+function exponent(base, exp) {
+    if (exp === 0) return 1;
+    return base * exponent(base, exp -1);
+}
+
+//version2
+function exponentV2(base, exp) {
+    if (exp === 0) return 1;
+    if (exp === 1) return base;
+
+    if (exp % 2 === 0) {
+        return (exponentV2(base, exp/2))**2;
+    }
+    return base * (exponentV2(base, (exp-1)/2)**2);
+}
+
+function fibonacci(n) {
+    if (n === 0) return [];
+    if (n === 1) return [1];
+    if (n === 2) return [1, 1];
+    
+    const temp = fibonacci(n-1);
+    return temp.concat(temp[n-1] + temp[n-2])
+}
+
+
+function deepDup(arr) {
+    const output = [];
+    arr.forEach((ele) => {
+        if (ele instanceof Array) { 
+            output.concat(deepDup(ele));
+        } else {
+            output.concat(ele);
+        }
+    }); 
+    return output;
+}
