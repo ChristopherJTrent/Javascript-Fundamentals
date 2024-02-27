4b2ec26e5935814dfb600ddc835cc2c0d7433099
ChristopherJtrent <ChristopherJtrent@outlook.com>
2024-02-27 14:40:44 -0800

    implement phase 3 and a bit of 4

--- phase-3-iteration.js
+++ phase-3-iteration.js
@@ -1,1 +1,35 @@
 // Your code here
+
+Array.prototype.bubbleSort = function() {
+    const swap = function(arr, index1, index2) {
+        const temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    let temp = [...this];
+    if (temp.length <= 1) return temp;
+    let swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (let i = 0; i < temp.length - 1; i++) {
+            if (temp[i] > temp[i+1]) {
+                // swap(temp, i, i+1);
+                const temp_storage = temp[i];
+                temp[i] = temp[i+1];
+                temp[i+1] = temp_storage;
+                swapped = true
+            }
+        }
+    }
+    return temp;
+}
+
+String.prototype.substrings = function() {
+    const arr = []
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i+1; j < this.length; j++) {
+            arr.push(this.slice(i, j));
+        }
+    }
+    return arr;
+}
--- phase-4-recursion.js
+++ phase-4-recursion.js
@@ -1,1 +1,19 @@
 // Your code here
+//? equivalent to (start...end) in ruby
+function range(start, end, accumulator = [])  {
+    //? start 
+    //? end
+    //? accumulator
+    //? the accumulator will be returned at the end as our array of numbers from start to end
+    //? base case:
+    //?     start === end
+    //?         return accumulator
+    //? recursive step: (if start < end)
+    //?     range(start+1, end, accumulator << start)
+    //! start === end will be exclusive
+    //! start > end will be inclusive
+    if (start > end) {
+        return accumulator;
+    }
+    return range(start + 1, end, accumulator.concat(start));
+}
