8381f9d23b959609b8b4ef1665bcc82fc6ebf783
ChristopherJtrent <ChristopherJtrent@outlook.com>
2024-02-27 16:32:01 -0800

    implement beginnings of phase 6

--- phase-5-cat.js
+++ phase-5-cat.js
@@ -1,1 +1,26 @@
 // Your code here
+
+function Cat(name, owner) {
+    this.name = name;
+    this.owner = owner;
+}
+
+Cat.prototype.cuteStatement = function() {
+    return `${this.owner} loves ${this.name}`;
+}
+
+const cat1 = new Cat("jackson", "diego");
+const cat2 = new Cat("tom", "jerry");
+console.log(cat1.cuteStatement());
+cat1.cuteStatement = function(){
+    return `Everyone loves ${this.name}`;
+}
+console.log(cat1.cuteStatement());
+console.log(cat2.cuteStatement());
+
+Cat.prototype.cuteStatement = function() {
+    return "This method has been overloaded";
+}
+
+console.log(cat1.cuteStatement());
+console.log(cat2.cuteStatement());
--- phase-6-students-and-courses.js
+++ phase-6-students-and-courses.js
@@ -1,1 +1,32 @@
 // Your code here
+/**
+ * 
+ * @param {String} fname first name
+ * @param {String} lname last name
+ */
+function Student(fname, lname) {
+    this.fname = fname;
+    this.lname = lname;
+    this.courses = [];   
+}
+
+function Course(name, dept, credits) {
+    this.name = name;
+    this.dept = dept;
+    this.credits = credits;
+    this.students = [];
+}
+
+Student.prototype.name = function() {
+    return `${this.fname} ${this.lname}`;
+}
+
+/**
+ * @param {Course} course 
+ */
+Student.prototype.enroll = function(course) {
+    this.courses.push(course)
+    course.students.push(this.name())
+}
+
+
